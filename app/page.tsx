d4b693a00193b533cc89366df229c04950303282
Sushant <srdarkseer@gmail.com>
2023-04-23 15:36:25 +0545

    weather-app

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#2C3333] to-[#010101] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">Powered by OpenAI</Subtitle>
+
+        <Divider className="my-10" />
+
+        <Card className="bg-gradient-to-br from-[#2C3333] to-[#010101]">
+          <CityPicker />
+        </Card>
+      </Card>
+    </div>
+  );
+}
---
+++ components/CityPicker.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Country, City } from "country-state-city";
+import Select from "react-select";
+
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}));
+
+function CityPicker() {
+  return <Select options={options} />;
+}
+
+export default CityPicker;
