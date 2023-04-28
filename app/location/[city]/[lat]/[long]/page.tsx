cf86468f63587d47b6fe28369655700877a51692
Sushant <srdarkseer@gmail.com>
2023-04-28 22:52:40 +0545

    Fetching weather queries from GraphQL

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from "@/apollo-client";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 type Props = {
   params: {
     city: string;
@@ -6,8 +9,36 @@ type Props = {
   };
 };
 
-function WeatherPage({params: {city, lat, long}}: Props) {
-  return <div>Welcome to Weather Page: {city} {lat} {long}</div>;
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: "GMT",
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+
+  return (
+    <div>
+      <div>
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Today's Overview</h2>
+            <p className="text-sm text-gray-400">Latest updated at: {" "}</p>
+            {new Date(results.current_weather.time).toLocaleString()} ({results.timezone})
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WeatherPage;
