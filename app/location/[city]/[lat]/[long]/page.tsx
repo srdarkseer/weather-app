4d1259ff4e07d5249018b35704e6ca1ca85cd3d4
Sushant <srdarkseer@gmail.com>
2023-04-30 19:24:03 +0545

    Created HumidityChart in weather page

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,5 +1,6 @@
 import { getClient } from "@/apollo-client";
 import CallOutCard from "@/components/CallOutCard";
+import HumidityChart from "@/components/HumidityChart";
 import InformationPanel from "@/components/InformationPanel";
 import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
@@ -87,6 +88,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
         <div className="space-y-3">
           <TempChart results={results} />
           <RainChart results={results} />
+          <HumidityChart results={results} />
         </div>
       </div>
     </div>
