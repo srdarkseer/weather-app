63ed0b3332cc83953b9d301a086a4b3af80742bc
Sushant <srdarkseer@gmail.com>
2023-05-01 16:58:36 +0545

    hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#C9EEEF] to-[#09A8FA] p-10 flex flex-col justify-center items-center">
       <Card className="max-w-4xl mx-auto">
-        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Text className="text-6xl font-bold text-center mb-10">Arcane Weather AI</Text>
         <Subtitle className="text-xl text-center">Powered by OpenAI</Subtitle>
 
         <Divider className="my-10" />
